7019f929860fa2b90e95a43a6f446177c6bbd6fd
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-06-24 11:51:53 +0300

    made API requests for counter

--- src/api(DAL)/api.ts
+++ src/api(DAL)/api.ts
@@ -4,13 +4,33 @@ const instance = axios.create({
     baseURL: process.env.API_BASE_URL
 })
 
-type CounterSettings ={
+type Counter = {
+    value: number
+}
+
+type CounterSettings = {
     maxValue: number,
     startValue: number
 }
 
-const counterSerwise = {
-    getCounterSettings(){
+export const counterServiceAPI = {
+    getCounter() {
+        return instance.get<Counter>("/counter").then(res => res.data)
+    },
+    updateCounter(value: number) {
+        return instance.put<Counter>("/counter", {value: value})
+    },
+    getCounterSettings() {
         return instance.get<CounterSettings>("/counter-settings").then(res => res.data)
+    },
+    updateCounterMaxValue(value: number) {
+        return instance.patch<CounterSettings>("/counter-settings", {maxValue: value})
+    },
+    updateCounterStartValue(value: number) {
+        return instance.patch<CounterSettings>("/counter-settings", {startValue: value})
     }
 }
+
+
+// @ts-ignore
+window.counterServiceAPI = counterServiceAPI
