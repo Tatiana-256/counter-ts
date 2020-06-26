6bb26012166ae1a2556cfdb3082d1759378ba42e
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-06-26 10:30:53 +0300

    add validations and styles for counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import {connect} from "react-redux";
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
 
 import './App.css';
 import {AppStateType} from "./redux-store(BLL)/store";
 import NavigationMenu from './components(UI)/header/NavMenu';
 import Counter from "./components(UI)/counter/Counter";
 import CounterSettings from "./components(UI)/counterSettings/CounterSettings";
-import {Route, Switch, Redirect} from 'react-router-dom';
+import {Redirect, Route, Switch} from 'react-router-dom';
+import {setCounterSettings} from "./redux-store(BLL)/settingsReducer";
+import {requestStatusType} from './redux-store(BLL)/appReducer';
 
 
 type PropsType = {}
 
 const App = (props: PropsType) => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(setCounterSettings())
+    }, [])
+
+    const requestStatus = useSelector<AppStateType, requestStatusType>(state => state.requestStatus.requestStatus)
+
     return (
         <div className="App">
-            <NavigationMenu/>
+            <NavigationMenu requestStatus={requestStatus}/>
             <Switch>
                 <Route path="/counter" component={Counter}/>
                 <Route path="/settings" component={CounterSettings}/>
--- src/components(UI)/counter/Counter.tsx
+++ src/components(UI)/counter/Counter.tsx
@@ -2,13 +2,16 @@ import React, {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {AppStateType} from '../../redux-store(BLL)/store';
 import {incrementCounterValue, setCounterValue} from '../../redux-store(BLL)/counterReducer';
-import styles from "../counterSettings/CounterSettings.module.css";
+import styles from "./Counter.module.css";
 
 
 type PropsType = {}
 
 const Counter = (props: PropsType) => {
     const value = useSelector<AppStateType, number>(state => state.count.value)
+    const startValue = useSelector<AppStateType, number>(state => state.settings.startValue)
+    const maxValue = useSelector<AppStateType, number>(state => state.settings.maxValue)
+
     const dispatch = useDispatch()
 
     useEffect(() => {
@@ -19,11 +22,21 @@ const Counter = (props: PropsType) => {
         dispatch(incrementCounterValue(value + 1))
     }
 
+    const resetStartValue = () => {
+        dispatch(incrementCounterValue(startValue))
+    }
+
+    const maxValueAchieved = value >= maxValue
+
     return (
         <div className={styles.setting}>
-            <div>Counter</div>
+            <div className={styles.counter}>Counter</div>
             <div>{value}</div>
-            <button onClick={increment}>Increment</button>
+            <div className={styles.buttons}>
+                <button onClick={increment} disabled={maxValueAchieved}>Increment</button>
+                <button onClick={resetStartValue}>Reset</button>
+            </div>
+            {maxValueAchieved && <div>Counter can`t be more than max value</div>}
         </div>
 
     );
